74324b01006ccec1a8aa079f5ca233527cdc1e27
besufikad <07yilmabese@gmail.com>
2023-10-07 10:36:36 +0300

    advanced_task 1

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,27 +3,50 @@
 The `deploy` function creates a compressed archive file of the
 `web_static` folder and saves it in the `versions` for deployment
 """
-from fabric.api import *
-env.hosts = ['52.201.220.122', '54.90.60.221']
 from datetime import datetime
 import os
+from fabric.api import *
+env.hosts = ['52.201.220.122', '54.90.60.221']
 
 
 def do_clean(number=0):
-    today = datetime.now()
+    """
+    The `do_clean` function cleans up old versions of a
+    web application and moves the most recent
+    versions to a shared directory.
+    """
     created_at = []
     for i in os.listdir("versions"):
-        created_at.append(datetime.fromtimestamp(os.stat("versions/" + i).st_ctime))
-    recent = []
-    latest = datetime(2000, 1, 9, 2, 34, 35)
+        times = datetime.fromtimestamp(os.stat("versions/" + i).st_ctime)
+        created_at.append(times)
+    created_at = sorted(created_at)
+    created_at = created_at[len(created_at) - 2:len(created_at)]
+    if number == 0 or number == 1:
+        created_at = [created_at[-1]]
+    # print(created_at)
+    for i in os.listdir("versions"):
+        times = datetime.fromtimestamp(os.stat("versions/" + i).st_ctime)
+        if times not in list(created_at):
+            local("rm versions/{}".format(i))
+            # print(i)
+    names = []
     for i in created_at:
-        if i > latest:
-            print("latest")
-            latest = i
-            if len(recent) < 2:
-                print("recent")
-                recent.append(latest)
-    
-
-
-do_clean()
+        names.append("web_static_" + str(i.year) + str(i.month) +
+                     str(i.day) + str(i.hour) + str(i.minute) +
+                     str(i.second))
+    run("cd /data/web_static/releases/")
+    try:
+        sudo("mv /data/web_static/releases/test /data/web_static/shared/")
+        sudo("mv /data/web_static/releases/{}\
+             /data/web_static/shared/".format(names[0]))
+        sudo("mv /data/web_static/releases/{}\
+             /data/web_static/shared/".format(names[1]))
+        sudo("rm -r /data/web_static/releases/*")
+        # sudo("mkdir /data/web_static/releases/")
+        # sudo("chown ubuntu:ubuntu releases")
+        sudo("mv /data/web_static/shared/* /data/web_static/releases")
+    except IndexError:
+        sudo("rm -r /data/web_static/releases/*")
+        sudo("mv /data/web_static/shared/* /data/web_static/releases")
+    # print(list_files)
+    # print(names[0])
