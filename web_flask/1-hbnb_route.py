dde1ca5a5fda5e79d78bd78079f85a97e980a7ae
besufikad <07yilmabese@gmail.com>
2023-10-19 16:51:52 +0300

    task 2 hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Module 0-hello_route
+A basic Flask app that writes "Hello, HBNB!" to the screen
+'''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root_route():
+    """
+    The function `root_route` returns the string "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+    The function hbnb_route returns the string "HBNB".
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
