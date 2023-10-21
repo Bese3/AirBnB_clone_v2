21b1d501745363a769fe36b4147d3b1b719e1e98
besufikad <07yilmabese@gmail.com>
2023-10-21 17:31:46 +0300

    t 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ flask module for states"""
 from models import storage
+from os import getenv
 from flask import Flask, render_template
 from models.state import State
 app = Flask(__name__)
@@ -14,7 +15,9 @@ def cities_by_states():
     with the sorted states."""
     all_state = storage.all(State)
     states = sorted(all_state.values(), key=lambda state: state.name)
-    return render_template("8-cities_by_states.html", states=states)
+    storage_type = getenv("HBNB_TYPE_STORAGE")
+    return render_template("8-cities_by_states.html", states=states,
+                           storage_type=storage_type)
 
 
 @app.teardown_appcontext
